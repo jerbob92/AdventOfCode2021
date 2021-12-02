3bba31c090c9e4b7ba96d7f10d4ac654926b1383
Jeroen Bobbeldijk <jeroen@klippa.com>
2021-12-02 09:47:02 +0100

    Use sliding window size, use buffer, keep track of history

--- day1/main.go
+++ day1/main.go
@@ -1,52 +1,66 @@
 package main
 
 import (
-	"bytes"
-	"io/ioutil"
+	"bufio"
 	"log"
+	"os"
 	"strconv"
 )
 
 func main(){
-	input, err := ioutil.ReadFile("day1/input.txt")
+	slidingWindowSize := 3
+
+	file, err := os.Open("day1/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
 
-	// Split on newlines
-	depthStrings := bytes.Split(input, []byte("\n"))
+	currentDepthIndex := 0
 
-	// Parse lines into numbers.
-	var depths = make([]int, len(depthStrings))
-	for i := range depthStrings {
-		depth, err := strconv.Atoi(string(depthStrings[i]))
+	// Keep track of amount of increments.
+	amountOfIncrements := 0
+	amountOfSlidingWindowIncrements := 0
+
+	// Keep track of previous values.
+	var previousDepth *int
+	slidingWindowHistory := make([]int, slidingWindowSize)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		depth, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			continue
 		}
-		depths[i] = depth
-	}
 
-	// Keep track of amount of increments
-	amountOfIncrements := 0
-	amountOfSlidingWindowIncrements := 0
-	slidingWindowSize := 3
-
-	for i := range depths {
-		// Compare to last depth.
-		if i > 0 && depths[i] > depths[i-1] {
+		if previousDepth != nil && depth > *previousDepth {
 			amountOfIncrements++
 		}
 
-		// Compare to sliding window.
-		if i >= slidingWindowSize {
-			previousSlidingWindowSize := depths[i-1] + depths[i-2] + depths[i-3]
-			currentSlidingWindowSize := depths[i] + depths[i-1] + depths[i-2]
+		previousDepth = &depth
+
+		// Compare previous window to current window.
+		if currentDepthIndex >= slidingWindowSize {
+			previousSlidingWindowSize := sumIntSlice(slidingWindowHistory)
+			currentSlidingWindowSize := (previousSlidingWindowSize - slidingWindowHistory[0]) + depth
 			if currentSlidingWindowSize > previousSlidingWindowSize {
 				amountOfSlidingWindowIncrements++
 			}
 		}
+
+		// Rewrite sliding window history
+		slidingWindowHistory = append(slidingWindowHistory[1:], depth)
+		currentDepthIndex++
 	}
 
 	log.Printf("Part 1: Amount of measurements larger than the previous measurement: %d", amountOfIncrements)
 	log.Printf("Part 2: Amount of sliding measurements window larger than the previous sliding measurements window: %d", amountOfSlidingWindowIncrements)
+}
+
+func sumIntSlice(slice []int) int {
+	total := 0
+	for _, value := range slice {
+		total += value
+	}
+	return total
 }
