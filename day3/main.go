ea2e96d39da95b3d7bd34d95e98ba7c62ab878b7
Jeroen Bobbeldijk <jeroen@klippa.com>
2021-12-03 10:21:22 +0100

    Dyanmic report size, add some comments

--- day3/main.go
+++ day3/main.go
@@ -14,12 +14,22 @@ func main() {
 	defer file.Close()
 
 	amountOfReports := 0
-	reportSize := 12
-	reportSums := make([]int, reportSize)
+	var reportSums []int
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		bits := scanner.Text()
+
+		// Initialize the array to keep track of the report sums.
+		// We assume every line has the same length.
+		if reportSums == nil {
+			reportSums = make([]int, len(bits))
+		}
+
+		// Loop through every report.
 		for i, char := range bits {
+			// When a bit is positive, add one to the current position in the
+			// report.
 			if string(char) == "1" {
 				reportSums[i]++
 			}
@@ -30,13 +40,17 @@ func main() {
 
 	epsilonRate := int64(0)
 	gammaRate := int64(0)
+
+	// Loop through every sum of the values in the report.
 	for pos, reportSum := range reportSums {
 		if reportSum > (amountOfReports / 2) {
-			epsilonRate |= (1 << (reportSize - pos - 1))
+			// If most reports on this position are positive, add one to epsilonRate.
+			epsilonRate |= 1 << (len(reportSums) - pos - 1)
 		} else {
-			gammaRate |= (1 << (reportSize - pos - 1))
+			// If most reports on this position are negative, add one to gammaRate.
+			gammaRate |= 1 << (len(reportSums) - pos - 1)
 		}
 	}
 
-	log.Printf("Epsilon rate: %b (%d), gamma rate: %b (%d), power consumtion: %d", epsilonRate, epsilonRate, gammaRate, gammaRate, epsilonRate*gammaRate)
+	log.Printf("Part 1: Epsilon rate: %b (%d), gamma rate: %b (%d), power consumtion: %d", epsilonRate, epsilonRate, gammaRate, gammaRate, epsilonRate*gammaRate)
 }
